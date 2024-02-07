a6dd79b60e3b04b3be67c4dc6b319b7b03001227
nicolettetombu <157637051+nicolettetombu@users.noreply.github.com>
2024-02-07 12:15:07 -0800

    Add files via upload

---
+++ python_decal/demo-yuh/today.py
@@ -0,0 +1,2 @@
+print(12467+200000000000000000000*890123/444+709273*304)
+#this represents the length of my schlong
---
+++ python_decal/touch functions.py
@@ -0,0 +1,6 @@
+def mult(x,y):
+    # slay is going to return the product of x and y
+    prod = x * y
+    return (prod)
+
+print(mult(50,392))
